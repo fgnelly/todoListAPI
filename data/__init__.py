bf0fcde84cb77c21977c042c15a2afa4c5c5b03a
fgnelly <greenapplegameplay@gmail.com>
2020-12-09 23:16:58 +0000

    Added database and server conifg and *.xml to gitignore

--- data/__init__.py
+++ data/__init__.py
@@ -1,7 +1,18 @@
 __all__ = ["User", "Task", "AppSession", "Folder", "ServerSetting"]
 from utility.HttpException import HttpException, HttpErrorType
 
-connectionString = "mysql+mysqlconnector://root:@localhost:3306/testdb"
+from utility import Config
+serverConfig = Config.Get('server-config.xml')
+if serverConfig is None:
+    print('Could not load server config, check if tags are correct and the file exists.')
+    exit()
+
+connectionString = 'mysql+mysqlconnector://{0}:{1}@{2}/{3}'.format(
+    serverConfig['databaseUser'],
+    serverConfig['databasePassword'] if serverConfig['databasePassword'] is not None else "",
+    serverConfig['databaseServer'],
+    serverConfig['databaseName']
+)
 
 from sqlalchemy import create_engine
 engine = create_engine(connectionString, echo=False)
@@ -11,7 +22,6 @@ Base = declarative_base()
 
 from sqlalchemy.orm import sessionmaker
 Session = sessionmaker(bind=engine)
-# session = Session()
 
 from contextlib import contextmanager
 @contextmanager
